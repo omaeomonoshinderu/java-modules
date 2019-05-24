fa003003e7c7280135572915e087878e1fd867b1
keithagroves <keith.groves@jointheleague.org>
2019-05-23 18:46:09 -0700

    Update propagate.sh

--- propagate.sh
+++ propagate.sh
@@ -3,6 +3,7 @@
 MODULES="/home/ec2-user/java-module-updater-DO_NOT_MOVE/java-modules"
 destination="/home/ec2-user/java-module-updater-DO_NOT_MOVE/destination"
 cd ${MODULES}
+git clean -fd
 exec 3>&1 4>&2
 trap 'exec 2>&4 1>&3' 0 1 2 3
 exec 1>log.out 2>&1

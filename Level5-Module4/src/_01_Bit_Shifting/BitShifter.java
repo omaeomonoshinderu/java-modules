4e13c5b792b4cdb24c326cf07f1844f1eca41f55
EC2 Default User <ec2-user@ip-172-31-21-29.us-west-1.compute.internal>
2019-05-26 17:03:25 -0700

    add Level5-Module4

---
+++ Level5-Module4/src/_01_Bit_Shifting/BitShifter.java
@@ -0,0 +1,17 @@
+package _01_Bit_Shifting;
+
+public class BitShifter {
+	public static void main(String[] args) {
+		// 1. Jot down the value of num in binary.
+		int num = 4;
+		int numShifted = num << 1;
+		
+		// 2. Print the value of numShifted, and convert that value to binary.
+		
+		// 3. Compare the two binary values. Can you figure out what the << operator is for?
+		
+		// 4. Try shifting num 3 places.
+		
+		// FYI: Binary values can be shifted to the right as well using the >> operator.	
+	}
+}

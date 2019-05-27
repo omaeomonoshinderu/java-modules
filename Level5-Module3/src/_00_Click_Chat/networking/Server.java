e07cf34aa455f247b5f854ce7c12d75a5087d4bb
EC2 Default User <ec2-user@ip-172-31-21-29.us-west-1.compute.internal>
2019-05-26 17:03:25 -0700

    add Level5-Module3

---
+++ Level5-Module3/src/_00_Click_Chat/networking/Server.java
@@ -0,0 +1,75 @@
+package _00_Click_Chat.networking;
+
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import javax.swing.JOptionPane;
+
+public class Server {
+	private int port;
+
+	private ServerSocket server;
+	private Socket connection;
+
+	ObjectOutputStream os;
+	ObjectInputStream is;
+
+	public Server(int port) {
+		this.port = port;
+	}
+
+	public void start(){
+		try {
+			server = new ServerSocket(port, 100);
+
+			connection = server.accept();
+
+			os = new ObjectOutputStream(connection.getOutputStream());
+			is = new ObjectInputStream(connection.getInputStream());
+
+			os.flush();
+
+			while (connection.isConnected()) {
+				try {
+					JOptionPane.showMessageDialog(null, is.readObject());
+					System.out.println(is.readObject());
+				}catch(EOFException e) {
+					JOptionPane.showMessageDialog(null, "Connection Lost");
+					System.exit(0);
+				}
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public String getIPAddress() {
+		try {
+			return InetAddress.getLocalHost().getHostAddress();
+		} catch (UnknownHostException e) {
+			return "ERROR!!!!!";
+		}
+	}
+
+	public int getPort() {
+		return port;
+	}
+
+	public void sendClick() {
+		try {
+			if (os != null) {
+				os.writeObject("CLICK SENT FROM SERVER");
+				os.flush();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ Level5-Module3/src/_02_Chat_Application/ChatApp.java
@@ -0,0 +1,9 @@
+package _02_Chat_Application;
+
+/*
+ * Using the Click_Chat example, write an application that allows a server computer to chat with a client computer.
+ */
+
+public class ChatApp {
+
+}

464139dc386bf442bf280ffcd97dda16df8f078d
jaLeague <jackie.arseneau@jointheleague.org>
2019-06-04 18:11:36 -0700

    Updated instructions

--- Level0-Module2/src/_01_intro_to_random/IntroToRandom.java
+++ Level0-Module2/src/_01_intro_to_random/IntroToRandom.java
@@ -8,20 +8,21 @@ import java.util.Random;
 public class IntroToRandom {
 	public static void main(String[] args) {
 		
-		int r = 0;
+		int num = 0;
 		
-		//3. Now make r random. 
-		//   Create an object of the Random class
-		//	 use .nextInt() to get a random number
+		//3   Now to make num random. 
+		//3a. Create an object of the Random class, Hint: Random ran = new Random()
+		
+		//3b. Next, set the value of num, using .nextInt() to get a random number from the Random object
 		
 
-		//4. Limit the random number between 0 and 100
+		//4. Now limit the value of num to be between 0 and 100
 
-		//5. Limit the random number between 25 and 75. Hint: ((highest value - lowest value)+1) + lowest value
+		//5. Now limit the value of num to be between 25 and 75. Hint: ((highest value - lowest value)+1) + lowest value
 
-		//6. Challenge: Limit the random number between -222 and 88
+		//6. Challenge: Limit the value of num to be between -222 and 88
 
-		//1. Print out the value of r
+		//1. Print out the value of num
 		
 		//2. Run the program.  What number appears in the console?
 		//   Run it again. Is the number the same?

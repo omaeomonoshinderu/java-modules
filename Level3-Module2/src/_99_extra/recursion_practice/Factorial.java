f16d7d70be4f12b54f58abb7cf5d407680067dbe
chmilk1(Home) <jamesmaron87@gmail.com>
2019-07-10 22:02:26 -0700

    Add Factorial class

---
+++ Level3-Module2/src/_99_extra/recursion_practice/Factorial.java
@@ -0,0 +1,14 @@
+package _99_extra.recursion_practice;
+
+public class Factorial {
+	public static int factorial(int n) {
+		//1. Make an if statment to check of n is zero
+			//2. if its zero return 1
+		
+			//3. else return n * factorial(n-1)
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("!5 is " + factorial(5));
+	}
+}

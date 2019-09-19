ede6a2ca4e65b7c0849b28d31a8b522179e3c989
tylercwiltz <twiltz@ucsd.edu>
2019-09-18 17:06:54 -0700

    Created 4 ReturnType Recipes

---
+++ Level0-Module5/src/_99_extra/ReturnTypesAdvanced.java
@@ -0,0 +1,51 @@
+package _99_extra;
+
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
+public class ReturnTypesAdvanced {
+	static Robot rob = new Robot();
+	
+	public static void main(String[] args) {
+		//1. Ask the user how many sides they want their shape to be
+		
+		//2. Call the canMakeShape() to see if the shape can be made with the number of sides entered
+		
+			//3. If it can, then call the appropriate method to get the angle the robot needs to turn each time
+	
+			//4. Call the appropriate method to draw the Shape
+		
+		//5. Else call the appropriate method and use a pop-up to tell the user they need to put a different number 
+	
+	}
+	
+	static int calculateTurnAngle(int numSides) {
+		int angle = 180-(((numSides-2)*180)/numSides);
+		return angle;
+	}
+	
+	static void drawPolygon(int numSides, int degrees) {
+		rob.setSpeed(100);
+		rob.penDown();
+		for(int i = 0; i < numSides; i++) {
+			rob.move(100);
+			rob.turn(degrees);
+		}
+		rob.hide();
+	}
+	
+	static boolean canMakeShape(int numSides) {
+		if(numSides >= 3) {
+			return true;
+		}
+		return false;
+	}
+	
+	static String notEnoughSides() {
+		return "I'm sorry we cannot make a shape with that many sides\n"
+				+ "Please enter a number greater than 2";
+	}
+}
---
+++ Level0-Module5/src/_99_extra/ReturnTypesPractice.java
@@ -0,0 +1,25 @@
+package _99_extra;
+
+public class ReturnTypesPractice {
+	public static void main(String[] args) {
+		//Call each method and using a syso print out what each method returns
+	
+	}
+	
+	static int method1() {
+		return HiddenTypes.unknown1;
+	}
+	static double method2() {
+		return HiddenTypes.unknown2;
+	}
+	static String method3() {
+		return HiddenTypes.unknown3;
+	}
+	static char method4() {
+		return HiddenTypes.unknown4;
+	}
+	static boolean method5() {
+		return HiddenTypes.unknown5;
+	}
+
+}

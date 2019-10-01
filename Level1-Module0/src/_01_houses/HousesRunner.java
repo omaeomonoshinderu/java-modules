8e762986a056720f8db8e63cdf6112963c49bae3
tylercwiltz <twiltz@ucsd.edu>
2019-10-01 14:20:24 -0700

    Update Houses Recipe to include a runner class

---
+++ Level1-Module0/src/_01_houses/HousesRunner.java
@@ -0,0 +1,11 @@
+package _01_houses;
+
+public class HousesRunner {
+
+	public static void main(String[] args) {
+		//Here we call the run() method from the Houses class
+		new Houses().run();
+
+	}
+
+}

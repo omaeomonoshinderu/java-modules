ee612b77ce0f7c974df719899b463fd16ad0285f
dave <dave@192.168.0.3>
2019-10-30 12:01:38 -0700

    Changed references to Templates to Generics in Level 4 Module 1.

--- Level4-Module1/src/_00_Intro_To_Generics/IntroToGenerics.java
+++ Level4-Module1/src/_00_Intro_To_Generics/IntroToGenerics.java
@@ -1,15 +1,15 @@
-package _00_Intro_To_Templates;
+package _00_Intro_To_Generics;
 
 import java.util.Random;
 
-// A template is a way to define one variable that can act as different types.
-// Templates can be used in methods and classes. 
-// The syntax for templates are the angle brackets <>
+// Generics in Java are a way to define one variable that can act as different types.
+// Generics can be used in methods and classes. 
+// The syntax for generics are the angle brackets <>
 
 
-public class IntroToTemplates {
+public class IntroToGenerics {
 	public static void main(String[] args) {
-		IntroToTemplates i = new IntroToTemplates();
+		IntroToGenerics i = new IntroToGenerics();
 		
 		String s = "taco";
 		//Here we are calling calcStuff and passing in a String
@@ -20,12 +20,12 @@ public class IntroToTemplates {
 		i.calcStuff(r, 6);
 		
 		//Here we create two object of the SomeClass class and 
-		//define its template variable to be two different types, String and Integer
+		//define its generic variable to be two different types, String and Integer
 		SomeClass<String> scObj = new SomeClass<String>("sodapop");
 		SomeClass<Integer> scObj2 = new SomeClass<Integer>(412);
 	}
 	
-	//Here a template called Template is created as part of the calcStuff method
+	//Here a generic called Template is created as part of the calcStuff method
 	//The variable t can be any type: String, Object, Robot... whatever
 	public <Template> Template calcStuff(Template t, int r) {
 		for (int i = 0; i < r; i++) {
@@ -36,8 +36,8 @@ public class IntroToTemplates {
 	}
 }
 
-//Here a class is created that has a template called AotherTemplate
-//Class templates must be a class, not a primitive data type: int, float, double, boolean, long, short, byte
+//Here a class is created that has a generic called AotherTemplate
+//Class generics must be a class, not a primitive data type: int, float, double, boolean, long, short, byte
 class SomeClass<AnotherTemplate>{
 	//Here the t variable can be any object type
 	AnotherTemplate t;

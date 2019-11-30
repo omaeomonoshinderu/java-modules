fc22397b5dec310b973076e571ca12b8a44cf4cf
ColbySchexnayder <schexnayderhusser@gmail.com>
2019-11-30 13:05:46 -0800

    Updated instructions for clarity

--- Level5-Module0/src/_03_To_Do_List/ToDoList.java
+++ Level5-Module0/src/_03_To_Do_List/ToDoList.java
@@ -4,8 +4,10 @@ public class ToDoList {
 	/*
 	 * Create a program with five buttons, add task, view tasks, remove task, save list, and load list. 
 	 * 
+	 * The progam should also have an input field and a submit button
+	 *
 	 * When add task is clicked:
-	 * 		ask the user for a  task and save it to an array list
+	 * 		ask the user for a task and save it to an array list
 	 * 
 	 * When the view tasks button is clicked:
 	 *		show all the tasks in the list
@@ -19,6 +21,10 @@ public class ToDoList {
 	 * When the load list button is clicked:
 	 * 		Prompt the user for the location of the file and load the list from that file
 	 * 
-	 * When the program starts, it should automatically load the last saved file into the list.
+	 * When the submit button is clicked:
+	 *		If the user has not clicked a button ask the user to do so
+	 *		If the user has clicked a button retrive the value from the input field and finish that buttons function
+	 *
+	 * When the program starts, it should automatically load the last saved file into the list. 
 	 */
 }

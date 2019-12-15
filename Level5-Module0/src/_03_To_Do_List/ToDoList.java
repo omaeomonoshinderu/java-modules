29e8954dfec23c37f4b808726fbd0ef2b63f6a5e
ColbySchexnayder <schexnayderhusser@gmail.com>
2019-12-15 11:37:18 -0800

    Clarified instructions to focus on JOptionPane for input
    -Discarded JTextfield and submit button instructions
    -Emphasized use of JOptionPane for receiving input
    - Capitalized ArrayList
    
    The solution for this currently uses JTextArea should it be updated to match this?

--- Level5-Module0/src/_03_To_Do_List/ToDoList.java
+++ Level5-Module0/src/_03_To_Do_List/ToDoList.java
@@ -3,28 +3,22 @@ package _03_To_Do_List;
 public class ToDoList {
 	/*
 	 * Create a program with five buttons, add task, view tasks, remove task, save list, and load list. 
-	 * 
-	 * The progam should also have an input field and a submit button
 	 *
 	 * When add task is clicked:
-	 * 		ask the user for a task and save it to an array list
+	 * 		Create a JOptionPane to ask the user for a task and save it to an ArrayList
 	 * 
 	 * When the view tasks button is clicked:
 	 *		show all the tasks in the list
 	 * 
 	 * When the remove task button is clicked:
-	 * 		prompt the user for which task to remove and take it off of the list.
+	 * 		Create a JOptionPane to prompt the user for which task to remove and take it off of the list.
 	 * 
 	 * When the save list button is clicked:
 	 * 		Save the list to a file
 	 * 
 	 * When the load list button is clicked:
-	 * 		Prompt the user for the location of the file and load the list from that file
+	 * 		Create a JOptionPane to Prompt the user for the location of the file and load the list from that file
 	 * 
-	 * When the submit button is clicked:
-	 *		If the user has not clicked a button ask the user to do so
-	 *		If the user has clicked a button retrive the value from the input field and finish that buttons function
-	 *
 	 * When the program starts, it should automatically load the last saved file into the list. 
 	 */
 }

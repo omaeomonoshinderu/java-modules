b101f021a26ec3ef85551f30c09d57d9bab0b91a
jaLeague <jackie.arseneau@jointheleague.org>
2020-04-27 14:57:21 -0700

    rework module 0

---
+++ Level0-Module0/src/src/_02_code_flow/_1_robot_square/RobotSquare.java
@@ -0,0 +1,38 @@
+package _02_code_flow._1_robot_square;
+/*
+ *    Copyright (c) The League of Amazing Programmers 2013-2020
+ *    Level 0
+ */
+
+import org.jointheleague.graphical.robot.Robot;
+
+// This recipe draws a square using the Robot
+
+public class RobotSquare {
+	
+    public static void main(String[] args){
+ 
+    	// This code makes a new Robot
+    	Robot rob = new Robot();
+
+        // Put the robot's pen down so it can draw
+		//        Use the     rob.penDown();   command.
+
+        // SPEED. Make the robot move quickly.
+		//        Use the     rob.setSpeed();   command.
+
+
+        // DRAW.  Draw a line
+    	//		Use the     rob.move(); command
+
+
+		// TURN.  Turn 90 degrees
+    	//      Use the     rob.turn();  command
+
+
+        // LOOP.  Now put the DRAW and TURN code into a for loop to repeat 4 times.
+		//        Did your Robot draw a square?
+
+
+    }
+}
---
+++ Level0-Module0/src/src/_02_code_flow/_2_robot_spiral/RobotSpiral.java
@@ -0,0 +1,35 @@
+package _02_code_flow._2_robot_spiral;
+/*
+ *    Copyright (c) The League of Amazing Programmers 2013-2020
+ *    Level 0
+ */
+
+
+import org.jointheleague.graphical.robot.Robot;
+
+public class RobotSpiral {
+
+	public static void main(String[] args) {
+		
+		// Create a new Robot
+		
+		// Set your robot's pen down 
+		
+		// SPEED. Set the robot to go at max speed (100)
+		
+        // LOOP. Use a for loop to repeat ALL the following lines of code 50 times. 
+		
+				// Change the robot pen color to random
+			
+				// DRAW.    Move the robot (5*i) pixels
+				//          "i" is the variable in the for loop	
+			
+				// TURN.    Turn the robot (360/7) degrees to the right
+			
+				// Change the robot pen width to (i)
+
+		
+		// Check the pattern against the picture in the recipe. If it matches, you are done!
+		
+	}
+}

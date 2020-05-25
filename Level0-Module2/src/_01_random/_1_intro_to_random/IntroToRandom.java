b0ab4bbe124f5671daad13f9fa767d7cde2891b9
jaLeague <jackie.arseneau@jointheleague.org>
2020-05-25 16:14:16 -0700

    intro and Magic 8 Ball

--- Level0-Module2/src/_01_random/_1_intro_to_random/IntroToRandom.java
+++ Level0-Module2/src/_01_random/_1_intro_to_random/IntroToRandom.java
@@ -8,23 +8,58 @@ import java.util.Random;
 public class IntroToRandom {
 	public static void main(String[] args) {
 		
-		int num = 0;
+		int number = 0;
 		
-		//3   Now to make num random. 
-		//3a. Create an object of the Random class, Hint: Random ran = new Random()
+		// Before you can make random numbers, you need a Random object. Random objects make random things.
+		// Random objects are created like this.
+		Random ran = new Random();
 		
-		//3b. Next, set the value of num, using .nextInt() to get a random number from the Random object
+		/*************  Random integers with no bounds  *************/
 		
+		// Run this program and you will see that nextInt created 5 random integers which have been printed to the console
+		for (int i=0; i<5; i++) {
+			number = ran.nextInt();
+			System.out.println(number);
+		}
+		// The range of numbers that an int variable can hold is very large (as you can see in the console).
+		// Integers can also be positive or negative.
 
-		//4. Now limit the value of num to be between 0 and 100
-
-		//5. Now limit the value of num to be between 25 and 75. Hint: ((highest value - lowest value)+1) + lowest value
-
-		//6. Challenge: Limit the value of num to be between -222 and 88
-
-		//1. Print out the value of num
+		/*************  Random positive integers with an upper limit only  *************/
 		
-		//2. Run the program.  What number appears in the console?
-		//   Run it again. Is the number the same?
+		// Usually we want to limit the numbers to a range in our program. 		
+		for (int i=0; i<5; i++) {
+			// Uncomment the line of code below, but replace the _____ with a positive integer (e.g. 100) before running the program			
+
+			// number = ran.nextInt(_____);
+			System.out.println(number);
+			
+		}
+		
+		// All the numbers printed by this loop are less then the number you added - AND POSITIVE
+		// This means, if you entered 100, all the numbers returned by nextInt are between 0 and 99 inclusive
+
+		
+		/*************  Random integers with an upper and lower limit  *************/
+		
+		// But what if you don't want the range to start at 0?  Let's say we want numbers between 25 and 75 inclusive.
+		// In this case, 25 is the lowest value we want, and 75 is the highest value. See below how to do this:
+		
+		for (int i=0; i<5; i++) {
+			// Uncomment the line of code below, and replace (_____)  with   (highest value - lowest value + 1)
+			// For our example, this will be:  ( 75 - 25 + 1 )
+			// Also replace <lowest value> with the start of the range.  For our example, this is 25.
+			
+			// number = ran.nextInt(_____) + <lowest value>;
+			System.out.println(number);
+			
+		}
+		// This time, all the values printed should be between 25 and 75.
+
+		
+		/*************  Random integers with an upper and lower (negative) limit  *************/
+		
+		// Using the above example, generate 5 random numbers between -222 and 88
+
+
 	}
 }

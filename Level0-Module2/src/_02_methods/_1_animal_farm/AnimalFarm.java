d0e57bf0cf133d2a17a9d0814ae1748a4acc03ad
KevinBarron92105 <jackie.arseneau@jointheleague.org>
2020-08-04 15:19:26 -0700

    add animal images

--- Level0-Module2/src/_02_methods/_1_animal_farm/AnimalFarm.java
+++ Level0-Module2/src/_02_methods/_1_animal_farm/AnimalFarm.java
@@ -1,62 +1,108 @@
-//    Copyright (c) The League of Amazing Programmers 2013-2019
+//    Copyright (c) The League of Amazing Programmers 2013-2020
 //    Level 0
 
 package _02_methods._1_animal_farm;
 
-import java.applet.AudioClip;
-
-//Copyright (c) The League of Amazing Programmers 2013-2017
-
-//Level 0
-
-import javax.swing.JApplet;
+import java.io.File;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
 
 public class AnimalFarm {
 	
+	/***********  SOUND ***************
+	 * Some computers are unable to play sounds. 
+	 * If you cannot play sound on this computer, set canPlaySounds to false.
+	 * If you are not sure, ask your teacher 
+	 * *****************/
+	boolean canPlaySounds = true;
+
+	
 	public static void main(String[] args) {
-			
-		/* 1. Ask the user which animal they want, then play the sound of that animal. */			 
+		new AnimalFarm().animals();
+	}
+	
+	void animals() {
+
+		/* 1. Ask the user which animal they want, then see and hear 
+		 *    the animal they chose using one of the methods below.
+		*/			 
 			
 		/* 2. Make it so that the user can keep entering new animals. */
 
 	}
 
-	static void playMoo() {
+	void moo() {
 		playNoise(mooFile);
+		showImage(cowImage);
 	}
 
-	static void playQuack() {
+	void quack() {
 		playNoise(quackFile);
+		showImage(duckImage);
 	}
 
-	static void playWoof() {
+	void woof() {
 		playNoise(woofFile);
+		showImage(dogImage);
 	}
 	
-	static void playMeow() {
+	void meow() {
 		playNoise(meowFile);
+		showImage(catImage);
 	}
 	
-	static void playLlama() {
+	void llamaScream() {
 		playNoise(llamaFile);
+		showImage(llamaImage);
 	}
+	
 
-	static String quackFile = "quack.wav";
-	static String mooFile = "moo.wav";
-	static String woofFile = "woof.wav";
-	static String meowFile = "meow.wav";
-	static String llamaFile = "llama.wav";
+
+	
 
 	/* Ignore this stuff */
+	String quackFile = "quack.wav";
+	String duckImage = "duck.jpg";
+	String mooFile = "moo.wav";
+	String cowImage = "cow.jpg";
+	String woofFile = "woof.wav";
+	String dogImage = "dog.jpg";
+	String meowFile = "meow.wav";
+	String catImage = "cat.jpg";
+	String llamaFile = "llama.wav";
+	String llamaImage = "llama.jpg";
 	
-	public static void playNoise(String soundFile) {
+	private void showImage (String imageFile) {
 		try {
-			AudioClip sound = JApplet.newAudioClip(AnimalFarm.class.getResource(soundFile));
-			sound.play();
-			Thread.sleep(3400);
-		} catch (Exception ex) {
-			ex.printStackTrace();
+			ImageIcon animalPic = new ImageIcon("src/_02_methods/_1_animal_farm/"+imageFile);
+			JOptionPane.showMessageDialog(null, "", "You chose", 0, animalPic);
+
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, "I don't know what that animal looks like");			
+		}
+		
+	}
+	
+	private void playNoise(String soundFile) {
+		if (canPlaySounds) {	
+			File sound = new File("src/_02_methods/_1_animal_farm/"+soundFile);
+			if (sound.exists()) {
+				new Thread(() -> {
+				try {
+					Clip clip = AudioSystem.getClip();
+					clip.open(AudioSystem.getAudioInputStream(sound));
+					clip.start();
+					Thread.sleep(clip.getMicrosecondLength()/1000);
+				}
+				catch (Exception e) {
+					System.out.println("Could not play this sound");
+				}}).start();
+	 		}
+			else {
+				System.out.println("File does not exist");
+			}
 		}
 	}
 }

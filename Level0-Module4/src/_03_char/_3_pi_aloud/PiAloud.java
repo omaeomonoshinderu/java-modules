b364be17fce210206cad9e4b8caa15769d1dec9d
KevinBarron92105 <jackie.arseneau@jointheleague.org>
2020-08-05 15:24:19 -0700

    no sound option and updated speak method

--- Level0-Module4/src/_03_char/_3_pi_aloud/PiAloud.java
+++ Level0-Module4/src/_03_char/_3_pi_aloud/PiAloud.java
@@ -4,7 +4,14 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class PiAloud {
-
+	/***********  SOUND ***************
+	 * Some computers are unable to play sounds. 
+	 * If you cannot play sound on this computer, set canPlaySounds to false.
+	 * If you are not sure, ask your teacher 
+	 * *****************/
+	boolean canPlaySounds = true;
+	
+	
 	// 1. Make a main method and make sure your program can run
 
 	// 2. Make a String variable to hold 20 digits of Pi. You could use http://www.piday.org/million/ to get this.
@@ -12,10 +19,14 @@ public class PiAloud {
 	// 3. Print out 3 digits of Pi. The first value is "pi.charAt(0)", the second is "pi.charAt(1)"
 	// Check the console shows 3.1
 
-	// 4. Print ALL the digits of of the Pi String (hint: use a loop)
+	// 4. Print ALL the digits of the Pi String, putting each digit on a new line  (hint: use a loop)
 
-	// 5. Use the speak() method below to speak each digit of Pi.
-
+	// 5. Skip this step if your computer cannot play sounds. 
+	//		if (canPlaySounds) {
+	//				 Use the speak() method to speak each digit of Pi.   
+	//		}
+	
+	
 	// [CHALLENGE]
 	// *6. Get a character from the user using the getInputFromUser() method
 	// *7. Compare the users' char to the next digit of Pi
@@ -26,10 +37,21 @@ public class PiAloud {
 	/********************  Use these methods. DON'T CHANGE THE CODE BELOW  ******************/
 
 	static void speak(char characterToSpeak) {
-		try {
-			Runtime.getRuntime().exec("say " + characterToSpeak).waitFor();
-		} catch (Exception e) {
-			e.printStackTrace();
+		
+		if (System.getProperty("os.name").contains("Windows")) {
+			String cmd = "PowerShell -Command \"Add-Type -AssemblyName System.Speech; (New-Object System.Speech.Synthesis.SpeechSynthesizer).Speak('"
+					+ characterToSpeak + "');\"";
+			try {
+				Runtime.getRuntime().exec(cmd).waitFor();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} else {
+			try {
+				Runtime.getRuntime().exec("say " + characterToSpeak).waitFor();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
 	}
 

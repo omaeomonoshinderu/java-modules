07f9f384d7b1e0ce6631134feff6d7385ca36fb9
KevinBarron92105 <jackie.arseneau@jointheleague.org>
2020-09-07 17:08:32 -0700

    minor instruction changes

--- Level1-Module3/src/_03_jars/_1_intro_to_jars/RobotInAJar.java
+++ Level1-Module3/src/_03_jars/_1_intro_to_jars/RobotInAJar.java
@@ -17,14 +17,14 @@ public class RobotInAJar {
 	/* Even if you did not make any syntax errors, there should be a red line under the word Robot.
 	 * This is because the type Robot is not part of the basic Java language. If you want to use 
 	 * a Robot in your code, you must import the Robot class and all its code.   */
-	
+
 	/* Hover your mouse over the word Robot, and Eclipse should offer you a "quick fix" like this: 
 	 * 								Import 'Robot' (java.awt) 
 	 * HOWEVER, even though Java has this Robot class (in its java.awt library), 
-	 * that is NOT the one we want to use. */
-
-	 /* The League has its own fabulous Robot (org.jointheleague.graphical.robot.Robot)
-	  * BUT.... Eclipse did not offer it to us as a "quick fix" because it is not yet in this project */ 
+	 * that is NOT the one we want to use. The League has its own fabulous Robot 
+	 * org.jointheleague.graphical.robot.Robot
+	 * BUT.... Eclipse did not offer it to you as a "quick fix" because it has not yet 
+	 * been built into this project */ 
 
 
 	 /* 3.  So first we need to get the League's Robot code (from GitHub) */
@@ -38,24 +38,22 @@ public class RobotInAJar {
 
 	 /* 4.  Put the Robot JAR into this project */
   	 /* HOW?
-	 *  Drag and drop the robot.jar file you just downloaded to your computer into the current project in
-	 *  Eclipse.
+	 *  Drag the robot.jar file you downloaded from GitHub and drop it onto Eclipse, onto the current Java project.
 	 */
 	
 	/* 5.  Add the Robot JAR to the project Build Path */	
 	/* 
-	 * The Robot JAR is in the project, but Eclipse can't find it until it is added to the Build Path.
+	 * The Robot JAR is in the project, but Eclipse can't use it until it is added to the Build Path.
 	 * HOW?
-	 * Right click anywhere on the current package. Select Build Path->Configure Build Path from the menu.
+	 * Right click anywhere on the current project. Select Build Path->Configure Build Path from the menu.
 	 * Click on the Libraries tab, and select "Add JAR".
-	 * Search the project for the robot.jar file, then add it to the path, apply and close.
+	 * Select the robot.jar file, then add it to the build path, apply and close.
 	 */
 	
-	/* 6. NOW you can import   org.jointheleague.graphical.robot.Robot  */	
-	/* If you hover your mouse over the word Robot, choose the new option from the "quick fix" list: 
+	/* 6. Now if you hover your mouse over the word Robot, you will se anew option from the "quick fix" list: 
 	 * 				Import 'Robot (org.jointheleague.graphical.robot.Robot)
-	 * If all the red lines have disappeared, complete the draw method below.
-	 * Run the program from the RobotInAJarRunner class.
+	 * Select it. If all the red lines have disappeared, you can now complete the draw method below.
+	 * Run the program from the RobotInAJarRunner class and a shape should be drawn.
 	 */
 	
 	public void draw() {

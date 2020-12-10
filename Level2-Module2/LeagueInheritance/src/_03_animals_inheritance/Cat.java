02ce54a5cf0f5e7cd6eece62c853e3cf7ef1e6ed
tylercwiltz <twiltz@ucsd.edu>
2020-12-09 19:28:28 -0800

    Removed Solution to _03_animals_inheritance

--- Level2-Module2/LeagueInheritance/src/_03_animals_inheritance/Cat.java
+++ Level2-Module2/LeagueInheritance/src/_03_animals_inheritance/Cat.java
@@ -1,10 +1,5 @@
 package _03_animals_inheritance;
 
-public class Cat extends Animal {
-
-	Cat(String name, String furColor, boolean isGirl) {
-		super(name, furColor, isGirl);
-		// TODO Auto-generated constructor stub
-	}
+public class Cat {
 
 }
--- Level2-Module2/LeagueInheritance/src/_03_animals_inheritance/Dog.java
+++ Level2-Module2/LeagueInheritance/src/_03_animals_inheritance/Dog.java
@@ -1,10 +1,5 @@
 package _03_animals_inheritance;
 
-public class Dog extends Animal {
-
-	Dog(String name, String furColor, boolean isGirl) {
-		super(name, furColor, isGirl);
-		// TODO Auto-generated constructor stub
-	}
-
+public class Dog {
+	
 }

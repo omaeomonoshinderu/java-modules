3f54bdac674c5bac8251a44f8a9083eaa1a0261b
Mike <zaelix@gmail.com>
2021-01-07 15:04:25 -0800

    L2M2-IntroToTimers: Removed solutions from recipe. Oops.

--- Level2-Module2/src/_07_timers/IntroToTimers.java
+++ Level2-Module2/src/_07_timers/IntroToTimers.java
@@ -28,7 +28,7 @@ public class IntroToTimers extends JPanel implements ActionListener, KeyListener
 	int speed = 30;
 	
 	// 1. Declare a Timer variable, but do not initialize it.
-	Timer timer;
+
 
 	public static void main(String[] args) {
 		IntroToTimers tg = new IntroToTimers();
@@ -44,14 +44,12 @@ public class IntroToTimers extends JPanel implements ActionListener, KeyListener
 		frame.addKeyListener(this);
 		
 		// 2. Set the time variable to maxTime
-		time = maxTime;
-		
+
 		// 3. Initialize timer to a new Timer object. For the first argument, use 1000/speed. For the second argument, use this
-		timer = new Timer(1000/speed, this);
-		
+
 		// 11. Run it! Notice how it doesn't update? That's because our timer isn't triggering the actionPerformed() method yet!
 		// 12. Make the timer start!
-		timer.start();
+
 		// 13. Run the program. Is it possible for you to win? 
 		// 14. Figure out how to make it harder or easier! (Which condition is most related to whether you win or not?)
 	}
@@ -59,12 +57,12 @@ public class IntroToTimers extends JPanel implements ActionListener, KeyListener
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// 8. Decrement time
-		time--;
-		// 9. if time is less than or equal to 0...
-		if(time <= 0) {
+
+		// 9. if time is less than or equal to 2...
+
 			// 10. Set time equal to maxTime
-			time = maxTime;
-		}
+
+		
 		repaint();
 	}
 
@@ -73,18 +71,16 @@ public class IntroToTimers extends JPanel implements ActionListener, KeyListener
 		int keyCode = e.getKeyCode();
 		
 		// 4. if keyCode is 32 (that's the spacebar key!)...
-		if(keyCode == 32) {
-			System.out.println(time);
+
+
 			// 5. if time is less than 2...
-			if(time < 2) {
+
 				// 6. Tell them they win!
-				JOptionPane.showMessageDialog(null, "You win!");
-			}
+
+			
 			// 7. Otherwise, set time equal to maxTime
-			else {
-				time = maxTime;
-			}
-		}
+
+		
 	}
 
 	@Override

e38ce76e9d4bd20f909d535548fb9bfdb520abe2
root <root@ip-172-31-21-29.us-west-1.compute.internal>
2021-08-04 09:22:12 -0700

    Automatic Update

---
+++ Level3-Module0/src/_03_Intro_to_2D_Arrays/_02_RowColumnSum.java
@@ -0,0 +1,14 @@
+package _03_Intro_to_2D_Arrays;
+
+public class _02_RowColumnSum {
+
+    public int getSumByRow(int[][] matrix, int row) {
+        
+        return 0;
+    }
+    
+    public int getSumByColumn(int[][] matrix, int col) {
+        
+        return 0;
+    }
+}

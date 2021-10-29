46658db5b073c4e4fe9eae1d9cf9a84583a1b0b9
ColbySchexnayder <schexnayderhusser@gmail.com>
2021-10-29 12:33:58 -0700

    L1M3 - NetflixQueue

--- Level1-Module3/src/_05_netflix/Movie.java
+++ Level1-Module3/src/_05_netflix/Movie.java
@@ -1,9 +1,9 @@
 package _05_netflix;
+
 /*
- *    Copyright (c) The League of Amazing Programmers 2013-2019
+ *    Copyright (c) The League of Amazing Programmers 2013-2021
  *    Level 1
  */
-
 
 
 public class Movie implements Comparable<Movie> {
@@ -11,6 +11,12 @@ public class Movie implements Comparable<Movie> {
 	private String title;
 	private int stars;
 
+	/**
+	 * Constructor for the movie
+	 * 
+	 * @param title The Title of the movie
+	 * @param stars The movies rating
+	 */
 	public Movie(String title, int stars) {
 		this.title = title;
 		this.stars = stars;
@@ -24,11 +30,18 @@ public class Movie implements Comparable<Movie> {
 		return this.stars;
 	}
 
+	/**
+	 * Printing this object will print with title and stars
+	 */
 	public String toString() {
 		return "\"" + title + "\" - " + stars + " stars";
 	}
 
-public String getTicketPrice() {
+	/**
+	 * 
+	 * @return The ticket price based on star rating
+	 */
+	public String getTicketPrice() {
 		if (this.stars > 2) {
 			return "That will be $12 please.";
 		} else if (this.title.contains("Twilight")) {
@@ -37,8 +50,12 @@ public String getTicketPrice() {
 			return "Don't waste your money on this horrible rubbish.";
 		}
 	}
- 
 
+	/**
+	 * When sorted as part of a collection 
+	 * @param otherMovie The movie being compared to this object
+	 * @return The difference between the rating of the other movie and this
+	 */
 	public int compareTo(Movie otherMovie) {
 		int compareQuantity = otherMovie.getRating();
 

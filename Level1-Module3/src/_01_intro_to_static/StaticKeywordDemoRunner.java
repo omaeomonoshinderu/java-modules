cd30f7142f72058c9e96633cd11e4f15ec530451
Daniel Commins <dencee@users.noreply.github.com>
2022-01-18 14:07:34 -0800

    Intro to static

--- Level1-Module3/src/_01_intro_to_static/StaticKeywordDemoRunner.java
+++ Level1-Module3/src/_01_intro_to_static/StaticKeywordDemoRunner.java
@@ -1,5 +1,8 @@
 package _01_intro_to_static;
 
+/*
+ * Instructions are in the StaticKeywordDemo class
+ */
 public class StaticKeywordDemoRunner {
     public static void main(String[] args) {
         new StaticKeywordDemo();

df8eace4919e068a985369b73a480744d8aadc7f
Oleks K <alexokorshak@gmail.com>
2022-06-27 17:47:57 -0700

    add note of function arguments (#313)
    recommended use of setSpeed, move, and turn was confusing due to empty parenthesis

--- Level0-Module0/src/_02_code_flow/_1_robot_square/RobotSquare.java
+++ Level0-Module0/src/_02_code_flow/_1_robot_square/RobotSquare.java
@@ -19,7 +19,7 @@ public class RobotSquare {
 		 /**     rob.penDown();    **/
 
         // SPEED. Make the robot move quickly. Use this command:
-		  /**     rob.setSpeed();    **/
+		  /**     rob.setSpeed(<speed>);    **/
  
     	// COUNT. Create an int variable that will count how many sides of the square we have drawn.
     	//        The start value will be zero because no sides have yet been drawn. Use this code:
@@ -27,11 +27,11 @@ public class RobotSquare {
     	
 
         // DRAW.  Draw a line. Use this command:
-    		  /**     rob.move();    **/
+    		  /**     rob.move(<distance>);    **/
   
 
 	// TURN.  Turn 90 degrees. Use this command:
-    		  /**     rob.turn();    **/
+    		  /**     rob.turn(<angle>);    **/
 
     	
     	// INCREASE COUNT. Add one to the number of sides that have now been drawn like this:
